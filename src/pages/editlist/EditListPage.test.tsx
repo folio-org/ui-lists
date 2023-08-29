ba754aa7c053edefe39eeb09f38c888e460cc711
Vasilev Sergey <ed66679@gmail.com>
2023-08-29 20:24:31 +0400

    Up coverage for edit list page

--- src/pages/editlist/EditListPage.test.tsx
+++ src/pages/editlist/EditListPage.test.tsx
@@ -4,9 +4,12 @@ import { QueryClientProvider } from 'react-query';
 import { screen, waitFor, within } from '@testing-library/dom';
 import user from '@testing-library/user-event';
 import { render } from '@testing-library/react';
+import { Response, Server } from 'miragejs';
 import { startMirage } from '../../../test/mirage';
 import { EditListPage } from './EditListPage';
 import { queryClient } from '../../../test/utils';
+import * as hooks from '../../hooks';
+import listDetailsRefreshed from '../../../test/data/listDetails.refreshed.json';
 
 const historyPushMock = jest.fn();
 
@@ -28,7 +31,7 @@ const renderEditListPage = () => {
   );
 };
 
-let server: any;
+let server: Server;
 
 beforeEach(async () => {
   jest.clearAllMocks();
@@ -186,32 +189,199 @@ describe('CreateList Page', () => {
           });
         });
       });
+    });
 
-      describe('Disable/Enable save button', () => {
-        describe('When user dont made any changes', () => {
-          it('it is expected to keep save button disabled', () => {
-            const saveButton = screen.getByRole('button', {
-              name: 'ui-lists.button.save'
-            });
+    describe('Delete list', () => {
+      describe('Success delete', () => {
+        it('is expected to redirect after successful deletion and call success message', async () => {
+          const showSuccessMessageHookMock = jest.fn();
 
+          jest.spyOn(hooks, 'useMessages').mockImplementation(() => ({
+            showSuccessMessage: showSuccessMessageHookMock,
+            showErrorMessage: jest.fn(),
+            showInfoMessage: jest.fn(),
+            showWarningMessage: jest.fn(),
+            showMessage: jest.fn()
+          }));
 
-            expect(saveButton).toBeDisabled();
+          const deleteButton = screen.getByRole('menuitem', {
+            name: /ui-lists.pane.dropdown.delete/i
           });
+
+          await user.click(deleteButton);
+
+          const conformationModal = screen.getByTestId('ConfirmationModal');
+
+          const confirmButton = within(conformationModal).getByRole('button', {
+            name: /ui-lists.list.modal.delete/i
+          });
+
+          await user.click(confirmButton);
+
+          await waitFor(() => expect(deleteButton).toBeDisabled());
+
+          expect(conformationModal).not.toBeInTheDocument();
+
+          await waitFor(() => expect(historyPushMock).toBeCalledWith('/lists'));
+          expect(showSuccessMessageHookMock).toBeCalled();
         });
+      });
+      describe('Failure delete', () => {
+        it('is expected to show error message', async () => {
+          server.delete('lists/:id', () => new Response(404));
 
-        describe('When user made changes', () => {
-          it('it is expected to enable save button', async () => {
-            const saveButton = screen.getByRole('button', {
-              name: 'ui-lists.button.save'
-            });
-            const nameField = screen.getByLabelText('ui-lists.create-list.main.list-name', {
-              selector: 'input'
-            });
+          const showErrorMessageMock = jest.fn();
 
-            await user.type(nameField, ' some text');
+          jest.spyOn(hooks, 'useMessages').mockImplementation(() => ({
+            showSuccessMessage: jest.fn(),
+            showErrorMessage: showErrorMessageMock,
+            showInfoMessage: jest.fn(),
+            showWarningMessage: jest.fn(),
+            showMessage: jest.fn()
+          }));
 
-            expect(saveButton).toBeEnabled();
+          const deleteButton = screen.getByRole('menuitem', {
+            name: /ui-lists.pane.dropdown.delete/i
           });
+
+          await user.click(deleteButton);
+
+          const conformationModal = screen.getByTestId('ConfirmationModal');
+
+          const confirmButton = within(conformationModal).getByRole('button', {
+            name: /ui-lists.list.modal.delete/i
+          });
+
+          await user.click(confirmButton);
+
+          await waitFor(() => expect(deleteButton).toBeDisabled());
+
+          expect(conformationModal).not.toBeInTheDocument();
+
+          await waitFor(() => expect(showErrorMessageMock).toBeCalled());
+        });
+      });
+    });
+
+    describe('Edit and save', () => {
+      describe('Success save', () => {
+        it('is expected to redirect after successful deletion and call success message', async () => {
+          const showSuccessMessageHookMock = jest.fn();
+          const updatedListObject = { ...listDetailsRefreshed, version: listDetailsRefreshed.version + 1 };
+
+          server.put('lists/:id', () => new Response(200, {}, updatedListObject));
+          jest.spyOn(hooks, 'useMessages').mockImplementation(() => ({
+            showSuccessMessage: showSuccessMessageHookMock,
+            showErrorMessage: jest.fn(),
+            showInfoMessage: jest.fn(),
+            showWarningMessage: jest.fn(),
+            showMessage: jest.fn()
+          }));
+
+          const saveButton = screen.getByRole('button', {
+            name: 'ui-lists.button.save'
+          });
+
+          const nameField = screen.getByLabelText('ui-lists.create-list.main.list-name', {
+            selector: 'input'
+          });
+
+          expect(saveButton).toBeDisabled();
+
+          await user.type(nameField, ' name');
+
+          expect(saveButton).toBeEnabled();
+
+          await user.click(saveButton);
+
+          await waitFor(() => expect(saveButton).toBeDisabled());
+
+          await waitFor(() => expect(historyPushMock).toBeCalledWith('/lists/list/id'));
+
+          const successMessage = JSON.stringify(showSuccessMessageHookMock.mock.lastCall);
+          expect(successMessage).toContain('ui-lists.callout.list.save.success');
+        });
+      });
+
+      describe('Fail save', () => {
+        it('is expected to redirect after successful deletion and call success message', async () => {
+          const showErrorMessageMock = jest.fn();
+
+          server.put('lists/:id', () => new Response(404, {}, { code: 'error_code' }));
+
+          jest.spyOn(hooks, 'useMessages').mockImplementation(() => ({
+            showSuccessMessage: jest.fn(),
+            showErrorMessage: showErrorMessageMock,
+            showInfoMessage: jest.fn(),
+            showWarningMessage: jest.fn(),
+            showMessage: jest.fn()
+          }));
+
+          const saveButton = screen.getByRole('button', {
+            name: 'ui-lists.button.save'
+          });
+
+          const nameField = screen.getByLabelText('ui-lists.create-list.main.list-name', {
+            selector: 'input'
+          });
+
+          expect(saveButton).toBeDisabled();
+
+          await user.type(nameField, ' name');
+
+          expect(saveButton).toBeEnabled();
+
+          await user.click(saveButton);
+
+          await waitFor(() => expect(saveButton).toBeDisabled());
+
+          await waitFor(() => expect(historyPushMock).not.toBeCalled());
+
+          await waitFor(() => expect(showErrorMessageMock).toBeCalled());
+          const errorMessage = JSON.stringify(showErrorMessageMock.mock.lastCall);
+
+          expect(errorMessage).toContain('ui-lists.error_code');
+        });
+      });
+
+      describe('Fail due changed version', () => {
+        it('is expected to show optimistic lock error', async () => {
+          const showErrorMessageHookMock = jest.fn();
+          const updatedListObject = { ...listDetailsRefreshed, version: listDetailsRefreshed.version + 1 };
+          server.get('lists/:id', () => new Response(200, {}, updatedListObject));
+
+          jest.spyOn(hooks, 'useMessages').mockImplementation(() => ({
+            showSuccessMessage: jest.fn(),
+            showErrorMessage: showErrorMessageHookMock,
+            showInfoMessage: jest.fn(),
+            showWarningMessage: jest.fn(),
+            showMessage: jest.fn()
+          }));
+
+          const saveButton = screen.getByRole('button', {
+            name: 'ui-lists.button.save'
+          });
+
+          const nameField = screen.getByLabelText('ui-lists.create-list.main.list-name', {
+            selector: 'input'
+          });
+
+          expect(saveButton).toBeDisabled();
+
+          await user.type(nameField, ' name');
+
+          expect(saveButton).toBeEnabled();
+
+          await user.click(saveButton);
+
+          await waitFor(() => expect(saveButton).toBeDisabled());
+
+          await waitFor(() => expect(historyPushMock).not.toBeCalled());
+
+          await waitFor(() => expect(showErrorMessageHookMock).toBeCalled());
+
+          const errorMessage = JSON.stringify(showErrorMessageHookMock.mock.lastCall);
+          expect(errorMessage).toContain('ui-lists.update-optimistic.lock.exception');
         });
       });
     });
--- src/pages/editlist/EditListPage.tsx
+++ src/pages/editlist/EditListPage.tsx
@@ -100,7 +100,6 @@ export const EditListPage:FC = () => {
     }
   );
 
-
   const closeHandler = () => {
     if (hasChanges) {
       setShowConfirmCancelEditModal(true);
