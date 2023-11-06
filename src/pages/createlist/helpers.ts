ec3ecfe5ea259e0012e535346c06b9c7e40d814c
Noah Overcash <novercash@ebsco.com>
2023-11-06 13:52:05 -0700

    Use plain .sort

--- src/pages/createlist/helpers.ts
+++ src/pages/createlist/helpers.ts
@@ -12,12 +12,15 @@ export const computeRecordTypeOptions = (
   };
 
   let options = entityTypes
-    .toSorted((a, b) => a.label.localeCompare(b.label))
     .map(({ id, label }) => ({
       label,
       value: id,
       selected: id === selected,
     })) as EntityTypeSelectOption[];
+
+  // EntityTypeSelectOption has label as ReactNode, but we just created it above
+  // where all these labels are string only, so we can safely coerce to string
+  options.sort((a, b) => (a.label as string).localeCompare(b.label as string));
 
   if (!selected) {
     options = [defaultPlaceholder, ...options];
