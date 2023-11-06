dcd8d42ccf919cefd232bce3faee920cd8f29930
Noah Overcash <novercash@ebsco.com>
2023-11-06 13:24:07 -0700

    [UILISTS-61] Sort record types for "create list" view

--- src/pages/createlist/helpers.ts
+++ src/pages/createlist/helpers.ts
@@ -1,18 +1,23 @@
-import { EntityTypeSelectOption, EntityTypeOption } from '../../interfaces';
+import { EntityTypeOption, EntityTypeSelectOption } from '../../interfaces';
 import { t } from '../../services';
 
-export const computeRecordTypeOptions = (entityTypes: EntityTypeOption[], selected = ''): EntityTypeSelectOption[] => {
+export const computeRecordTypeOptions = (
+  entityTypes: EntityTypeOption[],
+  selected = '',
+): EntityTypeSelectOption[] => {
   const defaultPlaceholder = {
     label: t('create-list.choose-record-type'),
     selected: false,
     value: '',
   };
 
-  let options = entityTypes.map(({ id, label }) => ({
-    label,
-    value: id,
-    selected: id === selected
-  })) as EntityTypeSelectOption[];
+  let options = entityTypes
+    .toSorted((a, b) => a.label.localeCompare(b.label))
+    .map(({ id, label }) => ({
+      label,
+      value: id,
+      selected: id === selected,
+    })) as EntityTypeSelectOption[];
 
   if (!selected) {
     options = [defaultPlaceholder, ...options];
