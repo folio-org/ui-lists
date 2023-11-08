4e361f4f01c0e8d69c95d1573116b8bc7408bd05
Noah Overcash <novercash@ebsco.com>
2023-11-08 10:16:34 -0500

    [UILISTS-61] Sort record types when creating lists (#64)
    * [UILISTS-61] Sort record types for "create list" view
    
    * Fix typo
    
    * Add some additional translation infrastructure
    
    * Update typescript (needed for .toSorted)
    
    * Revert "Update typescript (needed for .toSorted)"
    
    This reverts commit 561e66d55ed43760e5106a76eb708d7faba14ab4.
    
    * Use plain .sort
    
    ---------
    
    Co-authored-by: Ryan Berger <rberger@ebsco.com>

--- src/services/translation/tests/translation.test.tsx
+++ src/services/translation/tests/translation.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from '@testing-library/react';
 import { describe } from '@jest/globals';
-import { t, UI_LISTS_NAMESPACE } from '../translation';
+import { render, screen } from '@testing-library/react';
+import { createIntl } from 'react-intl';
+import { UI_LISTS_NAMESPACE, t, tString } from '../translation';
 
 describe('Wrapper for translation', () => {
-  it('is expected to return sting with prefix', () => {
+  it('is expected to return (JSX) key with prefix', () => {
     const translationKey = 'button-text';
     const translationKeyWithPrefix = `${UI_LISTS_NAMESPACE}.${translationKey}`;
     render(<button type="button">{t('button-text')}</button>);
@@ -14,7 +15,7 @@ describe('Wrapper for translation', () => {
 
     expect(button).toBeInTheDocument();
   });
-  it('is expected to return key with prefix and values', () => {
+  it('is expected to return (JSX) key with prefix and values', () => {
     const translationKey = `button-text-${JSON.stringify({ count: 1 })}`;
     const translationKeyWithPrefixAndValues = `${UI_LISTS_NAMESPACE}.${translationKey}`;
     render(<button type="button">{t('button-text', { count: 1 })}</button>);
@@ -24,5 +25,27 @@ describe('Wrapper for translation', () => {
     });
 
     expect(button).toBeInTheDocument();
+  });
+  it('is expected to return (string) key with prefix', () => {
+    const translationKey = 'test';
+    const intl = createIntl({
+      locale: 'en',
+      messages: {
+        'ui-lists.test': 'translated value',
+      },
+    });
+
+    expect(tString(intl, translationKey)).toEqual('translated value');
+  });
+  it('is expected to return (string) key with prefix and values', () => {
+    const translationKey = 'test';
+    const intl = createIntl({
+      locale: 'en',
+      messages: {
+        'ui-lists.test': 'count={count}',
+      },
+    });
+
+    expect(tString(intl, translationKey, { count: 1 })).toEqual('count=1');
   });
 });
