4e04256ed2009539a1327f8e36864d622b501e4b
Vasilev Sergey <ed66679@gmail.com>
2023-11-30 17:28:35 +0400

    Fix ts;

--- src/pages/lists/types.ts
+++ src/pages/lists/types.ts
@@ -1,15 +1,10 @@
-export type FilterConfigType = {
-  label: string,
-  name: string,
-  values: string[] | {name: string, displayName: string}[]
-}[];
-
-
 export type FilterOptionType = {
   label: string,
   name: string,
   values: string[] | {name: string, displayName: string}[]
 }
+
+export type FilterConfigType = FilterOptionType[];
 
 export type AppliedFiltersType = {
   [key: string | number]: FilterOptionType | boolean
