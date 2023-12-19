7ac45e02e1b7dbc27da8ed6b488d8a145bf2d9c8
Vasilev Sergey <ed66679@gmail.com>
2023-12-19 13:34:39 +0400

    Fix refresh issues

--- src/pages/copylist/CopyListPage.tsx
+++ src/pages/copylist/CopyListPage.tsx
@@ -1,17 +1,12 @@
 import React, { FC } from 'react';
-import {
-  Accordion,
-  AccordionSet,
-  Layout,
-  Loading
-} from '@folio/stripes/components';
+import { Accordion, AccordionSet, Layout, Loading } from '@folio/stripes/components';
 import { useHistory, useParams } from 'react-router-dom';
 import { HTTPError } from 'ky';
-import { useListDetails, useMessages, useInitRefresh, useCreateList } from '../../hooks';
+import { useCreateList, useInitRefresh, useListDetails, useMessages } from '../../hooks';
 import { computeErrorMessage, t } from '../../services';
-import { ConfigureQuery, ErrorComponent, MainListInfoForm, EditListLayout } from '../../components';
+import { ConfigureQuery, EditListLayout, ErrorComponent, MainListInfoForm } from '../../components';
 import { useCopyListFormState } from './hooks';
-import { FIELD_NAMES, ListsRecordBase } from '../../interfaces';
+import { FIELD_NAMES, ListsRecordBase, STATUS_VALUES } from '../../interfaces';
 import { HOME_PAGE_URL } from '../../constants';
 
 import css from './CopyListPage.module.css';
@@ -24,28 +19,39 @@ export const CopyListPage:FC = () => {
   const listName = listDetails?.name ?? '';
   const fqlQuery = listDetails?.fqlQuery ?? '';
 
-  const { initRefresh } = useInitRefresh({ onSuccess: (data) => {
-    history.push(`/lists/list/${data.listId}`);
-  } });
-  const { showSuccessMessage, showErrorMessage } = useMessages();
-  const { state, onValueChange } = useCopyListFormState(listDetails, loadingListDetails);
+  const redirectToNewList = (newListId: string) => {
+    history.push(`${HOME_PAGE_URL}/list/${newListId}`);
+  };
 
   const backToList = () => {
     history.push(`${HOME_PAGE_URL}/list/${id}`);
   };
+
+  const { initRefresh } = useInitRefresh({ onSuccess: (data) => {
+    redirectToNewList(data.listId);
+  } });
+  const { showSuccessMessage, showErrorMessage } = useMessages();
+  const { state, onValueChange } = useCopyListFormState(listDetails, loadingListDetails);
+
 
   const recordType = listDetails?.entityTypeId;
   const { saveList, isLoading } = useCreateList(
     {
       listObject: { ...state, fqlQuery, recordType },
       onSuccess: (list: ListsRecordBase) => {
-        if (list.id) {
-          showSuccessMessage({ message: t('callout.list.save.success', {
-            listName: state[FIELD_NAMES.LIST_NAME]
-          }) });
-
-          initRefresh(list.id);
+        if (!list.id) {
+          return;
         }
+
+        showSuccessMessage({ message: t('callout.list.save.success', {
+          listName: state[FIELD_NAMES.LIST_NAME]
+        }) });
+
+        if (state[FIELD_NAMES.STATUS] === STATUS_VALUES.INACTIVE || !fqlQuery) {
+          redirectToNewList(list.id);
+        }
+
+        initRefresh(list.id);
       },
       onError: (error: HTTPError) => {
         (async () => {
