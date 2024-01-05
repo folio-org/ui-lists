461cf9394dc83cdd30360ad59696715ff7e0519c
Noah Overcash <novercash@ebsco.com>
2024-01-05 12:01:37 -0500

    Use strings for ConfirmationModal heading

--- src/components/CancelEditModal/CancelEditModal.tsx
+++ src/components/CancelEditModal/CancelEditModal.tsx
@@ -1,19 +1,21 @@
 import { ConfirmationModal } from '@folio/stripes/components';
 import React, { FC } from 'react';
-import { t } from '../../services';
+import { useIntl } from 'react-intl';
+import { t, tString } from '../../services';
 
 type CancelEditModalProps = {
-  onCancel: () => void,
-  onKeepEdit: () => void,
-  open: boolean
-}
+  onCancel: () => void;
+  onKeepEdit: () => void;
+  open: boolean;
+};
 
 export const CancelEditModal: FC<CancelEditModalProps> = ({ onCancel, onKeepEdit, open }) => {
+  const intl = useIntl();
   return (
     <ConfirmationModal
       confirmLabel={t('list.modal.keep-edit')}
       cancelLabel={t('list.modal.cancel-edit')}
-      heading={t('list.model.sure-heading')}
+      heading={tString(intl, 'list.model.sure-heading')}
       message={t('list.modal.confirm-cancel-message')}
       onCancel={onCancel}
       onConfirm={onKeepEdit}
--- src/components/ConfirmDeleteModal/ConfirmDeleteModal.tsx
+++ src/components/ConfirmDeleteModal/ConfirmDeleteModal.tsx
@@ -1,21 +1,28 @@
 import { ConfirmationModal } from '@folio/stripes/components';
 import React, { FC } from 'react';
-import { t } from '../../services';
+import { useIntl } from 'react-intl';
+import { t, tString } from '../../services';
 
 type ConfirmDeleteModalProps = {
-  listName: string,
-  onCancel: () => void,
-  onConfirm: () => void,
-  open: boolean
-}
+  listName: string;
+  onCancel: () => void;
+  onConfirm: () => void;
+  open: boolean;
+};
 
+export const ConfirmDeleteModal: FC<ConfirmDeleteModalProps> = ({
+  listName,
+  onConfirm,
+  onCancel,
+  open,
+}) => {
+  const intl = useIntl();
 
-export const ConfirmDeleteModal: FC<ConfirmDeleteModalProps> = ({ listName, onConfirm, onCancel, open }) => {
   return (
     <ConfirmationModal
       buttonStyle="danger"
       confirmLabel={t('list.modal.delete')}
-      heading={t('list.modal.delete-list')}
+      heading={tString(intl, 'list.modal.delete-list')}
       message={t('list.modal.confirm-message', { listName })}
       onCancel={onCancel}
       onConfirm={onConfirm}
