e846199f32bfd10569b4a556fbd453488fb62fd0
Vasilev Sergey <ed66679@gmail.com>
2024-02-28 21:35:19 +0400

    Fix sonar

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -53,7 +53,10 @@ export const buildListsUrl = (url: string, request?: ListsRequest) => {
 };
 
 export const createColumnHash = (listColumns: string[]) => {
-  const sortedColumns = [...listColumns].sort();
+  const sortedColumns = [...listColumns].sort((a, b) => {
+    return a.localeCompare(b, 'en', { sensitivity: 'base' });
+  })
+
   return `${sortedColumns.join()}`
 }
 
