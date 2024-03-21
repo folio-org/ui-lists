b162c91bd82eb10014ddf5934bc715970d8e5e34
Noah Overcash <novercash@ebsco.com>
2024-03-21 16:27:51 -0400

    [UILISTS-121] Improve exception handling (#119)

---
+++ src/components/ErrorComponent/ErrorComponent.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import type { HTTPError } from 'ky';
+import { ErrorComponent } from './ErrorComponent';
+
+describe('ErrorComponent', () => {
+  it.each([undefined, null])('renders nothing when no error is present (%s)', (error) => {
+    const { container } = render(<ErrorComponent error={error as unknown as HTTPError} />);
+
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it.each([
+    {},
+    { response: null },
+    { response: { json: () => Promise.resolve(null) } },
+    { response: { json: () => Promise.resolve('foo') } },
+    { response: { json: () => Promise.resolve({}) } },
+    { response: { json: () => Promise.resolve({ notAnError: [] }) } },
+  ])('handles improperly-formed HTTPError %s gracefully', async (error) => {
+    render(<ErrorComponent error={error as HTTPError} />);
+
+    await waitFor(() => expect(screen.queryByText('ui-lists.error-component.unknown')).toBeVisible());
+  });
+
+  it('handles properly-formed HTTPError gracefully', async () => {
+    render(
+      <ErrorComponent
+        error={{ response: { json: () => Promise.resolve({ code: 'code' }) } } as HTTPError}
+      />,
+    );
+
+    await waitFor(() => expect(screen.queryByText('ui-lists.error-component.code')).toBeVisible());
+  });
+});
--- src/components/ErrorComponent/ErrorComponent.tsx
+++ src/components/ErrorComponent/ErrorComponent.tsx
@@ -1,19 +1,24 @@
 import React, { FC, useEffect, useState } from 'react';
-// @ts-ignore:next-line
 import { Layout, Headline } from '@folio/stripes/components';
 import { HTTPError } from 'ky';
 import { t, parseErrorPayload } from '../../services';
 
 import css from './ErrorComponents.module.css';
 
-export const ErrorComponent:FC<{error: HTTPError}> = ({ error }) => {
+export const ErrorComponent: FC<{ error: HTTPError }> = ({ error }) => {
   const [errorKey, setErrorKey] = useState('');
 
   useEffect(() => {
-    parseErrorPayload(error)
-      .then(({ code }) => {
-        setErrorKey(code);
-      });
+    (async () => {
+      if (error) {
+        const payload = await parseErrorPayload(error);
+        if (payload?.code) {
+          setErrorKey(payload.code);
+        } else {
+          setErrorKey('unknown');
+        }
+      }
+    })();
   }, [error]);
 
   if (!errorKey) {
@@ -22,9 +27,7 @@ export const ErrorComponent:FC<{error: HTTPError}> = ({ error }) => {
 
   return (
     <section className={css.errorTitleWrapper}>
-      <Layout
-        className="display-flex flex-align-items-center centerContent"
-      >
+      <Layout className="display-flex flex-align-items-center centerContent">
         <Headline size="x-large" tag="h1" faded>
           {t(`error-component.${errorKey}`)}
         </Headline>
