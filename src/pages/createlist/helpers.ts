a25fb9bab2c21af090914c5847b4a831176e58f2
Vasilev Sergey <ed66679@gmail.com>
2024-04-26 00:07:10 +0400

    Remove outdated functionality

--- src/pages/createlist/helpers.ts
+++ src/pages/createlist/helpers.ts
@@ -1,16 +1,9 @@
 import { EntityTypeOption, EntityTypeSelectOption } from '../../interfaces';
-import { t } from '../../services';
 
 export const computeRecordTypeOptions = (
   entityTypes: EntityTypeOption[],
   selected = '',
 ): EntityTypeSelectOption[] => {
-  const defaultPlaceholder = {
-    label: t('create-list.choose-record-type'),
-    selected: false,
-    value: '',
-  };
-
   let options = entityTypes
     .map(({ id, label }) => ({
       label,
@@ -21,10 +14,6 @@ export const computeRecordTypeOptions = (
   // EntityTypeSelectOption has label as ReactNode, but we just created it above
   // where all these labels are string only, so we can safely coerce to string
   options.sort((a, b) => (a.label as string).localeCompare(b.label as string));
-
-  if (!selected) {
-    options = [defaultPlaceholder, ...options];
-  }
 
   return options;
 };
