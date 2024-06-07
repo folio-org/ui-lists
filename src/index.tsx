d636845c92c6781700ad93d62f65a11d1a3f7e0c
Vasilev Sergey <ed66679@gmail.com>
2024-06-07 14:18:34 +0400

    Add default shortcuts modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { IfPermission } from '@folio/stripes/core';
-
+import { IfPermission, AppContextMenu } from '@folio/stripes/core';
+import {
+  CommandList,
+  HasCommand,
+  KeyboardShortcutsModal,
+  NavList,
+  NavListItem,
+  NavListSection,
+  checkScope,
+  defaultKeyboardShortcuts,
+} from '@folio/stripes/components';
 import { ListPage, ListInformationPage, CreateListPage, EditListPage, CopyListPage } from './pages';
 import { USER_PERMS } from './utils/constants';
+import {t} from "./services";
 
 interface IListsApp {
   match: {
@@ -14,60 +24,111 @@ interface IListsApp {
 
 export const queryClient = new QueryClient();
 
-
+console.log(defaultKeyboardShortcuts);
 export const ListsApp: FC<IListsApp> = (props) => {
+  const [showKeyboardShortcutsModal, setShowKeyboardShortcutsModal] = useState(false);
+
+  const shortcutModalToggle = (handleToggle: () => {}) => {
+    handleToggle();
+    setShowKeyboardShortcutsModal(true);
+  };
+
+  const shortcuts = [
+    {
+      name: 'search',
+      handler: () => {
+        alert('hi')
+      }
+    },
+    {
+      name: 'openShortcutModal',
+      handler: setShowKeyboardShortcutsModal
+    },
+  ];
+
   const { match: { path } } = props;
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Switch>
-        <Route
-          path={path}
-          exact
-          render={() => (
-            <IfPermission perm={USER_PERMS.ReadList}>
-              <ListPage />
-            </IfPermission>
-          )}
-        />
-        <Route
-          path={`${path}/list/:id`}
-          exact
-          render={() => (
-            <IfPermission perm={USER_PERMS.ReadList}>
-              <ListInformationPage />
-            </IfPermission>
-          )}
-        />
-        <Route
-          path={`${path}/list/:id/edit`}
-          exact
-          render={() => (
-            <IfPermission perm={USER_PERMS.UpdateList}>
-              <EditListPage />
-            </IfPermission>
-          )}
-        />
-        <Route
-          path={`${path}/list/:id/copy`}
-          exact
-          render={() => (
-            <IfPermission perm={USER_PERMS.UpdateList}>
-              <CopyListPage />
-            </IfPermission>
-          )}
-        />
-        <Route
-          path={`${path}/new`}
-          exact
-          render={() => (
-            <IfPermission perm={USER_PERMS.CreateList}>
-              <CreateListPage />
-            </IfPermission>
-          )}
-        />
-      </Switch>
-    </QueryClientProvider>
+    <>
+      <CommandList commands={defaultKeyboardShortcuts}>
+        <HasCommand
+          commands={shortcuts}
+          isWithinScope={checkScope}
+          scope={document.body}
+        >
+          <AppContextMenu>
+            {(handleToggle: () => {}) => (
+              <NavList>
+                <NavListSection>
+                  <NavListItem
+                    id="shortcuts"
+                    onClick={() => { shortcutModalToggle(handleToggle); }}
+                  >
+                    {t('app-menu.keyboard-shortcuts')}
+                  </NavListItem>
+                </NavListSection>
+              </NavList>
+            )}
+          </AppContextMenu>
+          <QueryClientProvider client={queryClient}>
+            <Switch>
+              <Route
+                path={path}
+                exact
+                render={() => (
+                  <IfPermission perm={USER_PERMS.ReadList}>
+                    <ListPage />
+                  </IfPermission>
+                )}
+              />
+              <Route
+                path={`${path}/list/:id`}
+                exact
+                render={() => (
+                  <IfPermission perm={USER_PERMS.ReadList}>
+                    <ListInformationPage />
+                  </IfPermission>
+                )}
+              />
+              <Route
+                path={`${path}/list/:id/edit`}
+                exact
+                render={() => (
+                  <IfPermission perm={USER_PERMS.UpdateList}>
+                    <EditListPage />
+                  </IfPermission>
+                )}
+              />
+              <Route
+                path={`${path}/list/:id/copy`}
+                exact
+                render={() => (
+                  <IfPermission perm={USER_PERMS.UpdateList}>
+                    <CopyListPage />
+                  </IfPermission>
+                )}
+              />
+              <Route
+                path={`${path}/new`}
+                exact
+                render={() => (
+                  <IfPermission perm={USER_PERMS.CreateList}>
+                    <CreateListPage />
+                  </IfPermission>
+                )}
+              />
+            </Switch>
+            {showKeyboardShortcutsModal && (
+              <KeyboardShortcutsModal
+                allCommands={defaultKeyboardShortcuts}
+                onClose={() => { setShowKeyboardShortcutsModal(false); }}
+                open
+              />
+            )}
+          </QueryClientProvider>
+        </HasCommand>
+      </CommandList>
+    </>
   );
 };
 
