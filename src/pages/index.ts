04caa158adf0893d744e1e39976259a6a4794534
Noah Overcash <novercash@ebsco.com>
2024-06-20 17:29:15 -0400

    [UILISTS-128] Add message when attempting to access the lists app with no entity type permissions

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export { CreateListPage } from './createlist';
 export { ListInformationPage } from './listInformation';
 export { EditListPage } from './editlist';
 export { CopyListPage } from './copylist';
+export { MissingAllEntityTypePermissionsPage } from './missingperms';
