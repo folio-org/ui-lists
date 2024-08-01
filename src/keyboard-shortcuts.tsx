d6f7cb74c59e335b7d9eddc7f30ab94a5835cf67
Vasilev Sergey <ed66679@gmail.com>
2024-08-01 17:12:06 +0400

    Fix case

--- src/keyboard-shortcuts.tsx
+++ src/keyboard-shortcuts.tsx
@@ -11,11 +11,11 @@ const shortcutItem = (labelKey: string, shortcut: string) => {
 
 export const commandsGeneral = [
   shortcutItem('commands-label.create', 'Option + n'),
-  shortcutItem('commands-label.edit', 'cmd + Option + e'),
-  shortcutItem('commands-label.save', 'cmd + s'),
+  shortcutItem('commands-label.edit', 'Cmd + Option + e'),
+  shortcutItem('commands-label.save', 'Cmd + s'),
   shortcutItem('commands-label.duplicate', 'Option + C'),
   shortcutItem('commands-label.toggle-lists-detail-accordion', 'spacebar'),
-  shortcutItem('commands-label.expand-list-detail-accordions', 'cmd  + Option + b'),
-  shortcutItem('commands-label.collapse-list-detail-accordions', 'cmd  + Option + g'),
-  shortcutItem('commands-label.go-to-filter-pane', 'cmd + s'),
+  shortcutItem('commands-label.expand-list-detail-accordions', 'Cmd  + Option + b'),
+  shortcutItem('commands-label.collapse-list-detail-accordions', 'Cmd  + Option + g'),
+  shortcutItem('commands-label.go-to-filter-pane', 'Cmd + s'),
 ];
