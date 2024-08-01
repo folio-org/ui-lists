3fb38b7e532c7430af2603286799aa5ab878d930
Vasilev Sergey <ed66679@gmail.com>
2024-08-01 16:31:39 +0400

    Added shortcut keys custom list

---
+++ src/keyboard-shortcuts.tsx
@@ -0,0 +1,36 @@
+import { t } from './services';
+
+export const commandsGeneral = [
+  {
+    label: (t('commands-label.create')),
+    shortcut: 'Option + n'
+  },
+  {
+    label: (t('commands-label.edit')),
+    shortcut: 'cmd + Option + e'
+  },
+  {
+    label: (t('commands-label.save')),
+    shortcut: 'cmd + s'
+  },
+  {
+    label: (t('commands-label.duplicate')),
+    shortcut: 'Option + C'
+  },
+  {
+    label: (t('commands-label.toggle-lists-detail-accordion')),
+    shortcut: 'spacebar'
+  },
+  {
+    label: (t('commands-label.expand-list-detail-accordions')),
+    shortcut: 'cmd  + Option + b'
+  },
+  {
+    label: (t('commands-label.collapse-list-detail-accordions')),
+    shortcut: 'cmd  + Option + g'
+  },
+  {
+    label: (t('commands-label.go-to-filter-pane')),
+    shortcut: 'cmd + Option + h'
+  }
+];
