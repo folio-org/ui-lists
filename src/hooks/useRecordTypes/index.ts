ba564333434d6e83bf764019cd538f0143ad55a0
Vasilev Sergey <ed66679@gmail.com>
2024-08-30 16:12:13 +0400

    Change record type format

---
+++ src/hooks/useRecordTypes/index.ts
@@ -0,0 +1,1 @@
+export { useRecordTypes } from './useRecordTypes';
--- src/hooks/useRecordTypes/useRecordTypes.ts
+++ src/hooks/useRecordTypes/useRecordTypes.ts
@@ -1,12 +1,12 @@
 import { useQuery } from 'react-query';
 import { useOkapiKy } from '@folio/stripes/core';
-import { EntityType } from '../interfaces';
+import { EntityTypesResponse } from '../../interfaces';
 
 const ENTITY_TYPE_HASH = 'entityType';
 
 export const useRecordTypes = () => {
   const ky = useOkapiKy();
-  const { data, isLoading, error } = useQuery<EntityType[], Error>({
+  const { data, isLoading, error } = useQuery<EntityTypesResponse, Error>({
     queryKey: [ENTITY_TYPE_HASH],
     queryFn: async () => {
       const response = await ky.get('entity-types');
@@ -17,7 +17,7 @@ export const useRecordTypes = () => {
   });
 
   return ({
-    recordTypes: data,
+    recordTypes: data?.entityTypes || [],
     isLoading,
     error
   });
