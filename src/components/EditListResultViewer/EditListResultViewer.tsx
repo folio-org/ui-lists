9b275b7a93efef84b7b1df5770c4834e8bbeafd2
Noah Overcash <novercash@ebsco.com>
2024-09-20 11:25:07 -0400

    [UIPQB-120] Fix Query Builder resetting on RTR refresh

--- src/components/EditListResultViewer/EditListResultViewer.tsx
+++ src/components/EditListResultViewer/EditListResultViewer.tsx
@@ -1,6 +1,5 @@
-// @ts-ignore
 import { Pluggable, useOkapiKy } from '@folio/stripes/core';
-import React, { FC } from 'react';
+import React, { FC, useCallback, useMemo } from 'react';
 import { useVisibleColumns } from '../../hooks';
 import { QueryBuilderColumnMetadata, STATUS_VALUES, VISIBILITY_VALUES } from '../../interfaces';
 import { t } from '../../services';
@@ -9,64 +8,63 @@ import { ConfigureQuery } from '../ConfigureQuery';
 import css from './EditListResultViewer.module.css';
 
 interface EditListResultViewerProps {
-  id: string,
-  version?: number,
-  entityTypeId?: string,
-  fqlQuery: string,
-  userFriendlyQuery: string,
-  contentVersion: number,
-  fields?: string[],
-  status: string,
-  listName: string,
-  visibility: string,
-  description: string,
-  isDuplicating?: boolean,
-  isQueryButtonDisabled?: boolean,
+  id: string;
+  version?: number;
+  entityTypeId?: string;
+  fqlQuery: string;
+  userFriendlyQuery: string;
+  contentVersion: number;
+  fields?: string[];
+  status: string;
+  listName: string;
+  visibility: string;
+  description: string;
+  isDuplicating?: boolean;
+  isQueryButtonDisabled?: boolean;
   setColumns?: (columns: QueryBuilderColumnMetadata[]) => void;
 }
 
-export const EditListResultViewer:FC<EditListResultViewerProps> = (
-  {
-    id,
-    version,
-    entityTypeId,
-    fqlQuery,
-    userFriendlyQuery = '',
-    contentVersion,
-    status,
-    listName,
-    visibility,
-    description,
-    fields,
-    setColumns,
-    isDuplicating = false,
-    isQueryButtonDisabled = false
-  }
-) => {
+export const EditListResultViewer: FC<EditListResultViewerProps> = ({
+  id,
+  version,
+  entityTypeId,
+  fqlQuery,
+  userFriendlyQuery = '',
+  contentVersion,
+  status,
+  listName,
+  visibility,
+  description,
+  fields,
+  setColumns,
+  isDuplicating = false,
+  isQueryButtonDisabled = false,
+}) => {
   const ky = useOkapiKy();
 
-  const {
-    visibleColumns,
-  } = useVisibleColumns(id);
+  const { visibleColumns } = useVisibleColumns(id);
 
-  const getAsyncContentData = ({ limit, offset }: any) => {
-    return ky.get(`lists/${id}/contents?offset=${offset}&size=${limit}&fields=${visibleColumns?.join(',')}`).json();
-  };
+  const getAsyncContentData = useCallback(
+    ({ limit, offset }: { limit: number; offset: number }) =>
+      ky
+        .get(
+          `lists/${id}/contents?offset=${offset}&size=${limit}&fields=${visibleColumns?.join(',')}`,
+        )
+        .json(),
+    [ky, id, visibleColumns],
+  );
 
-  const getAsyncEntityType = () => {
-    return ky.get(`entity-types/${entityTypeId}`).json();
-  };
+  const getAsyncEntityType = useCallback(
+    () => ky.get(`entity-types/${entityTypeId}`).json(),
+    [ky, entityTypeId],
+  );
 
   return (
     <Pluggable
       type="query-builder"
       componentType="viewer"
-      accordionHeadline={
-        t('accordion.title.query',
-          { query: userFriendlyQuery })}
-      headline={({ totalRecords }: any) => (
-        t('mainPane.subTitle',
-          { count: totalRecords }))}
+      accordionHeadline={t('accordion.title.query', { query: userFriendlyQuery })}
+      headline={({ totalRecords }: any) => t('mainPane.subTitle', { count: totalRecords })}
       refreshTrigger={contentVersion}
       contentDataSource={getAsyncContentData}
       entityTypeDataSource={getAsyncEntityType}
@@ -74,23 +72,27 @@ export const EditListResultViewer:FC<EditListResultViewerProps> = (
       onSetDefaultColumns={setColumns}
       contentQueryKeys={visibleColumns}
       height={500}
-      additionalControls={(
+      additionalControls={
         <div className={css.queryBuilderButton}>
           <ConfigureQuery
             listId={isDuplicating ? undefined : id}
             version={isDuplicating ? undefined : version}
             isEditQuery={!isDuplicating}
-            initialValues={fqlQuery ? JSON.parse(fqlQuery) : undefined}
+            initialValues={useMemo(() => (fqlQuery ? JSON.parse(fqlQuery) : undefined), [fqlQuery])}
             selectedType={entityTypeId}
             recordColumns={fields}
             isQueryButtonDisabled={isQueryButtonDisabled}
             listName={listName}
             status={status === STATUS_VALUES.ACTIVE ? STATUS_VALUES.ACTIVE : STATUS_VALUES.INACTIVE}
-            visibility={visibility === VISIBILITY_VALUES.SHARED ? VISIBILITY_VALUES.SHARED : VISIBILITY_VALUES.PRIVATE}
+            visibility={
+              visibility === VISIBILITY_VALUES.SHARED
+                ? VISIBILITY_VALUES.SHARED
+                : VISIBILITY_VALUES.PRIVATE
+            }
             description={description}
           />
         </div>
-      )}
+      }
     >
       {t('loading-fallback')}
     </Pluggable>
