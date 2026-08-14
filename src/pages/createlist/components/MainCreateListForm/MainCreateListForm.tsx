e1f2ccdc82713927c94262d34f138479e59cae55
vashjs <vadym_shchekotilin@epam.com>
2026-08-14 12:54:46 +0200

    UILISTS-255 Remove the keyboard-inaccessible Collapse all action from the Create a new list page

--- src/pages/createlist/components/MainCreateListForm/MainCreateListForm.tsx
+++ src/pages/createlist/components/MainCreateListForm/MainCreateListForm.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from 'react';
 // @ts-ignore:next-line
-import { AccordionSet, Accordion, TextLink } from '@folio/stripes/components';
+import { AccordionSet, Accordion } from '@folio/stripes/components';
 import { t } from '../../../../services';
 import { ChangedFieldType, STATUS, VISIBILITY, EntityTypeSelectOption } from '../../../../interfaces';
 import { MainListInfoForm, ConfigureQuery } from '../../../../components';
@@ -36,26 +36,8 @@ export const MainCreateListForm:FC<MainCreateListFormProps> = (
     setIsOpened((prevValue) => !prevValue);
   };
 
-  const renderCollapseButton = () => {
-    return (
-      <TextLink onClick={onToggleHandler}>
-        {isOpened ?
-          (
-            t('create-list.main.collapse-all')
-          )
-          :
-          (
-            t('create-list.main.expand-all')
-          )
-        }
-      </TextLink>);
-  };
-
   return (
     <div>
-      <div className={css.mainFormCollapseButton}>
-        {renderCollapseButton()}
-      </div>
       <AccordionSet>
         <Accordion
           label={t('create-list.main.list-information')}
